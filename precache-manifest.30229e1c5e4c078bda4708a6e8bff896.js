2192c64deeaa978986212d8b19a5ffe44778cdb3
sourabh chauhan <sourabh1202@gmail.com>
2020-08-29 02:54:43 +0530

    Updates

---
+++ precache-manifest.30229e1c5e4c078bda4708a6e8bff896.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3a7fcf3b846c295911fe198cfaa569",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d41f8ee3f5c1859e6688",
+    "url": "/portfolio/static/css/main.9eff2eea.chunk.css"
+  },
+  {
+    "revision": "67d8defdf670c6155dd3",
+    "url": "/portfolio/static/js/2.7ae803b4.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/portfolio/static/js/2.7ae803b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41f8ee3f5c1859e6688",
+    "url": "/portfolio/static/js/main.f7c7899c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a0e9ccf0f99bd4b8a413c4263206366a",
+    "url": "/portfolio/static/media/Contact.a0e9ccf0.jpg"
+  },
+  {
+    "revision": "d2ccde95c45f3ecabc7e897a1bc008ec",
+    "url": "/portfolio/static/media/Item4.d2ccde95.png"
+  },
+  {
+    "revision": "89b8ee402fe41ab5beeffb2819e7df72",
+    "url": "/portfolio/static/media/Logo.89b8ee40.png"
+  },
+  {
+    "revision": "492d59255740ff0bc520de381e6e21af",
+    "url": "/portfolio/static/media/item1.492d5925.png"
+  },
+  {
+    "revision": "2bbd725cb9824e3a88eeed2b0a2b36cf",
+    "url": "/portfolio/static/media/item2.2bbd725c.png"
+  },
+  {
+    "revision": "dcf6829d860872afeca7bd5c05843f19",
+    "url": "/portfolio/static/media/item3.dcf6829d.png"
+  },
+  {
+    "revision": "e3de26c92105777350b9b0880e0546ab",
+    "url": "/portfolio/static/media/item5.e3de26c9.png"
+  },
+  {
+    "revision": "9aa9053e9e35d2b013d79027bb09aca3",
+    "url": "/portfolio/static/media/item6.9aa9053e.jpg"
+  },
+  {
+    "revision": "b59a2752c55f257aabdc81c4ed2c61f5",
+    "url": "/portfolio/static/media/item7.b59a2752.png"
+  },
+  {
+    "revision": "71768ef1ffe02c249e6008243c2f78b8",
+    "url": "/portfolio/static/media/item8.71768ef1.jpg"
+  },
+  {
+    "revision": "85cd5e0b980a5baa61f348adea9e96a9",
+    "url": "/portfolio/static/media/item9.85cd5e0b.png"
+  },
+  {
+    "revision": "023ab602b7405510611a5fc7383d94d4",
+    "url": "/portfolio/static/media/showcase.023ab602.jpg"
+  }
+]);
