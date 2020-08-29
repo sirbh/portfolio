4d1aaaece802e0f15c97078bb0fa5b9101a39f76
sourabh chauhan <sourabh1202@gmail.com>
2020-08-30 01:59:14 +0530

    corretion in contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,9 +7,9 @@ const Contact = ()=>
         <div className={cssClasses.Header}></div>
         <div className={cssClasses.Content}>
               <h1>Phone&nbsp;&nbsp;&nbsp;&nbsp;:<span>+91 9650446110</span></h1>
-              <h1>E-mail&nbsp;&nbsp;&nbsp;&nbsp;:<span>sourabh1202@gmail.com</span></h1>
-              <h1>GitHub&nbsp;&nbsp;&nbsp;:<a href="https://github.com/sirbh/">https://github.com/sirbh/</a></h1>
-              <h1>Website&nbsp;:</h1>
+              <h1>E-mail&nbsp;&nbsp;&nbsp;:<span>sourabh1202@gmail.com</span></h1>
+              <h1>GitHub&nbsp;&nbsp;&nbsp;:<a href="https://github.com/sirbh/">github.com/sirbh</a></h1>
+              <h1>Website&nbsp;&nbsp;:<a href="https://www.saurabhchauhan.in/">www.saurabhchauhan.in</a></h1>
         </div>
     </div>
 }
